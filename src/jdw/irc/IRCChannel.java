879fbcecb7f5b4c16efaba90d48fb01f586e3d24
daniel <daniel@daniel-Aspire-5750G>
2014-02-03 16:54:04 +0100

    Added doc to IRCChannel.java

--- src/jdw/irc/IRCChannel.java
+++ src/jdw/irc/IRCChannel.java
@@ -1,21 +1,45 @@
 package jdw.irc;
 
-
+/**
+ * Represents a Channel.
+ * 
+ * @author daniel
+ *
+ */
 public class IRCChannel extends IRCUserCollection {
 	private String name;	
 	
+	/**
+	 * Creates a Channel.
+	 * 
+	 * @param name The channels name.
+	 */
 	public IRCChannel(String name) {
 		this.name = name;		
 	}
 
+	/**
+	 * @return The name.
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Changes the channels name.
+	 * 
+	 * @param name The new name.
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 	
+	/**
+	 * Mathes the channels name against the given pattern.
+	 *  
+	 * @param regex A pattern to match with.
+	 * @return true if it matches, false otherwise.
+	 */
 	public boolean match(String regex) {
 		return name.matches(regex);
 	}
