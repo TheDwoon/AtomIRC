9cb0837684e8f56fad0547ac5bd6ec3500804daa
daniel <daniel@daniel-Aspire-5750G>
2014-02-03 17:02:41 +0100

    Added doc to IRCUser.java

--- src/jdw/irc/IRCUser.java
+++ src/jdw/irc/IRCUser.java
@@ -1,5 +1,12 @@
 package jdw.irc;
 
+/**
+ * Represents a user.
+ * Each user could have a nick, user and a host.
+ * 
+ * @author daniel
+ *
+ */
 public class IRCUser {
 	private String nick;	
 	private String user;
@@ -29,72 +36,143 @@ public class IRCUser {
 		return user;		
 	}
 	
+	/**
+	 * Creates a new user with no nick(null), no user(null) and no host(null).
+	 */
 	protected IRCUser() {
 		this(null, null, null);
 	}
 	
+	/**
+	 * Creates a new user.
+	 * 
+	 * @param nick Its nick.
+	 * @param user Its user.
+	 * @param host Its host.
+	 */
 	protected IRCUser(String nick, String user, String host) {
 		this.nick = nick;
 		this.user = user;
 		this.host = host;
 	}
 	
+	/**
+	 * Matches the composed userstring "<nick>!<user>@<host>" with the given pattern.
+	 * 
+	 * @param regex The pattern.
+	 * @return True if it machtes, false otherwise.
+	 */
 	public boolean match(String regex) {
 		return toString().matches(regex);
 	}
 	
+	/**
+	 * Matches the users nick with the given pattern.
+	 * 
+	 * @param regex The pattern.
+	 * @return True if it matches, false otherwise.
+	 */
 	public boolean matchNick(String regex) {
 		return (nick == null) ? false : nick.matches(regex);
 	}
 	
+	/**
+	 * Matches the users user with the given pattern.
+	 * 
+	 * @param regex The pattern.
+	 * @return True if it matches, false otherwise.
+	 */
 	public boolean matchUser(String regex) {
 		return (user == null) ? false : user.matches(regex);
 	}
 	
+	/**
+	 * Matches the users host with the given pattern.
+	 * 
+	 * @param regex The pattern.
+	 * @return True if it matches, false otherwise.
+	 */
 	public boolean matchHost(String regex) {
 		return (host == null) ? false : host.matches(regex);
 	}
 	
+	/**
+	 * @return True if the nick is not null and not empty, false otherwise.
+	 */
 	public boolean hasNick() {
 		return nick != null && !nick.isEmpty();
 	}
 	
+	/**
+	 * @return True if the user is not null and not empty, false otherwise.
+	 */
 	public boolean hasUser() {
 		return user != null && !user.isEmpty();
 	}
 	
+	/**
+	 * @return True if the host is not null and not empty, false otherwise.
+	 */
 	public boolean hasHost() {
 		return host != null && !host.isEmpty();
 	}
 	
+	/**
+	 * @return True if the nick, user and host are set to not null and are not empty, false otherwise.
+	 */
 	public boolean isComplete() {
 		return hasNick() && hasUser() && hasHost();
 	}
 	
+	/**
+	 * @return The nick.
+	 */
 	public String getNick() {
 		return nick;
 	}
 	
+	/**
+	 * Changes the nick.
+	 * 
+	 * @param nick New nick.
+	 */
 	public void setNick(String nick) {
 		this.nick = nick;
 	}
 	
+	/**
+	 * @return The user.
+	 */
 	public String getUser() {
 		return user;
 	}
 	
+	/**
+	 * Changes the user.
+	 * 
+	 * @param user New user.
+	 */
 	public void setUser(String user) {
 		this.user = user;
 	}
 	
+	/**
+	 * @return The host.
+	 */
 	public String getHost() {
 		return host;
 	}
 	
+	/**
+	 * Changes the host.
+	 * 
+	 * @param host New host.
+	 */
 	public void setHost(String host) {
 		this.host = host;
 	}	
 	
+	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 	
